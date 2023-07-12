65f016c738ad4557dfc099b436d3cd282f15cc39
santosh <gsantosh7456@gmail.com>
2023-07-12 16:48:43 +1000

    Initial commit

---
+++ src/main/java/com/example/jdblspringdemo/JdblSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdblspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdblSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdblSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jdblspringdemo/JdblSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdblspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdblSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
