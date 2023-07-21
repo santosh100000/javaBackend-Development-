518a43a3217c49d3d89c329d10ece61489294261
santosh <gsantosh7456@gmail.com>
2023-07-21 23:02:17 +1000

    demonstrated how to use jdbc to interact with rdms , using mysql

---
+++ L07-jdbc-demo/src/main/java/com/example/L07jdbcdemo/ProductService.java
@@ -0,0 +1,85 @@
+package com.example.L07jdbcdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductDAO productDAO;
+
+    private Integer nextId = 1;
+
+    private List<Product> dataStore = new ArrayList<>();
+
+    public ProductService() {
+        dataStore.add(new Product(nextId, "laptop"));
+        nextId++;
+    }
+
+    public List<Product> getAllProducts(){
+
+//        return dataStore;
+        return productDAO.getAllProducts();
+    }
+
+
+
+    public Product createProducts(Product product){
+        product.setId(nextId);
+        nextId++;
+        dataStore.add(product);
+        return product;
+    }
+
+//    public Product getById(Integer id){
+//        Product product = null;
+//        for (Product prod:dataStore){
+//            if (prod.getId().equals(id)){
+//                product= prod;
+//                break;
+//            }
+//        }
+//
+//        return product;
+//    }
+
+    //using db
+    public Product getById(Integer id){
+        Product product = productDAO.getProductById(id);
+        return product;
+    }
+
+
+
+    public Product updateProduct(Integer id, Product product) {
+        Product oldProduct = null;
+        for(Product prod:dataStore){
+            if(prod.getId() == id){
+                oldProduct = prod;
+                break;
+            }
+        }
+        if(oldProduct != null){
+            oldProduct.setName(product.getName());
+            oldProduct.setId(id);
+        }
+        return oldProduct;
+    }
+
+    public Product deleteProduct(Integer id) {
+        Product oldProduct = null;
+        for(Product prod:dataStore){
+            if(prod.getId() == id){
+                oldProduct = prod;
+                break;
+            }
+        }
+        dataStore.remove(oldProduct);
+        return oldProduct;
+    }
+}
