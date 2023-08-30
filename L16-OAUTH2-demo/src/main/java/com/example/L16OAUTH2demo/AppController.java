e5d940538a092d5718e77132819b5a5a155a6c98
santosh <gsantosh7456@gmail.com>
2023-08-30 21:06:37 +1000

    implemented oAuth2

--- L16-OAUTH2-demo/src/main/java/com/example/L16OAUTH2demo/AppController.java
+++ L16-OAUTH2-demo/src/main/java/com/example/L16OAUTH2demo/AppController.java
@@ -1,8 +1,13 @@
 package com.example.L16OAUTH2demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 
@@ -12,4 +17,20 @@ public class AppController {
     public ResponseEntity<String> hello(){
         return ResponseEntity.ok("Hello from "+Thread.currentThread().getName());
     }
+
+    @GetMapping("/user")
+    public Map<String, Object> user(){
+       OAuth2User user = (OAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+       Map<String, Object> response = new HashMap<>();
+       response.put("name", user.getAttributes().get("name") );
+       response.put("company", user.getAttributes().get("name"));
+       return response;
+    }
+
+    @GetMapping("/user-details")
+    public Map<String, Object> userDetails(){
+        OAuth2User user = (OAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return user.getAttributes();
+    }
+
 }
