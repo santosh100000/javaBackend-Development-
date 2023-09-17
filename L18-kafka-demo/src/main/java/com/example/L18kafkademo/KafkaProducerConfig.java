9b64b83e01262802e44ee4025741c2c48a68afb3
santosh <gsantosh7456@gmail.com>
2023-09-18 00:03:13 +1000

    setting up kafka

---
+++ L18-kafka-demo/src/main/java/com/example/L18kafkademo/KafkaProducerConfig.java
@@ -0,0 +1,35 @@
+package com.example.L18kafkademo;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+
+    @Bean
+    public ProducerFactory getProducerFactory(){
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return  new DefaultKafkaProducerFactory(configs);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> getKafkaTemplate(){
+        return new KafkaTemplate<>(getProducerFactory());
+
+    }
+}
